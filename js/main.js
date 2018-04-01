1004f9734758484856771f1af060c438ec85124c
calcala21 <37969028+calcala21@users.noreply.github.com>
2018-03-31 19:56:03 -0700

    First website upload

---
+++ js/main.js
@@ -0,0 +1,209 @@
+// Get modal element
+;(function (){ 
+  'use strict';
+var modal = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+  }());
+  
+  // Get modal element
+;(function (){ 
+  'use strict';
+var modal = document.getElementById('simpleModal1');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn1');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn1')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+  }());
+  
+
+   // Get modal element
+;(function (){ 
+  'use strict';
+var modal = document.getElementById('simpleModal2');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn2');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn2')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+  }());
+  
+
+
+   // Get modal element
+;(function (){ 
+  'use strict';
+var modal = document.getElementById('simpleModal3');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn3');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn3')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+  }());
+  
+
+
+   // Get modal element
+;(function (){ 
+  'use strict';
+var modal = document.getElementById('simpleModal4');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn4');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn4')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+  }());
+  
+
+   // Get modal element
+;(function (){ 
+  'use strict';
+var modal = document.getElementById('simpleModal5');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn5');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn5')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+	}());
